f896a156bfadc08689202209c443ca0f4ad4ae50
Istvan Viczian <viczian.istvan@gmail.com>
2022-02-17 18:48:09 +0100

    Naplózás

--- jdbc/demos/src/main/resources/db/migration/V1__employees.sql
+++ jdbc/demos/src/main/resources/db/migration/V1__employees.sql
@@ -1,1 +1,1 @@
-create table employees(id bigint auto_increment, emp_name varchar(255), constraint pk_employees primary key(id));
+create table employees(id bigint auto_increment, emp_name varchar(255), primary key(id));
